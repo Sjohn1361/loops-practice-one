2db3de85aec43ad9b57c40a888eeeb240ba95c20
JohnsonS <JohnsonS@T29683.niaccit.niacc.edu>
2020-10-15 09:39:00 -0500

    added some code

--- public_html/LoopsPractice1.js
+++ public_html/LoopsPractice1.js
@@ -4,3 +4,10 @@
 
 document.write("The numbers from 1 to 10 are: <br>");
 
+let Counter = 1;
+while(Counter  <=10){
+        document.write(Counter + "<br>");
+        Counter = Counter + 1; // this is a comment
+
+}
+document.write("Goodbye");
